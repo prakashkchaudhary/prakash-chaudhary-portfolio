2d8e85d73c89fc862af5334e70772b8dee039d35
prakashkchaudhary <prakashchaudhary92290@gmail.com>
2026-04-29 11:45:50 +0530

    Add Vercel Speed Insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './context/ThemeContext.jsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
+      <SpeedInsights />
     </ThemeProvider>
   </React.StrictMode>,
 )
